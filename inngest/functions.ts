0391142a165b42fdd5378efc3db8865ea8da76b8
vivekgorantla <23eg105j66@anurag.edu.in>
2026-02-09 04:45:06 +0530

    e2b sandbox setup + nextjs template created and pushed

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,9 +1,15 @@
 import { inngest } from "./client";
 import { createAgent, gemini } from '@inngest/agent-kit';
+import Sandbox from "@e2b/code-interpreter"
+import { getSandbox } from "@/lib/sandbox";
 export const codeAgent = inngest.createFunction(
     { id: "code-agent" },
     { event: "code-agent/codeAgent.run" },
     async ({ event, step }) => {
+        const sandboxId = await step.run('get-or-create-sandbox',async()=>{
+            const {sandboxId} = await Sandbox.create("23eg105j66/vibecreation-v1")
+            return sandboxId
+        })
         const summarizerAgent = createAgent({
             name: 'Summarizer',
             description: 'You Summarize long professional docs',
@@ -18,6 +24,12 @@ export const codeAgent = inngest.createFunction(
         });
 
         const {output} = await summarizerAgent.run(`summarize the below text: ${event.data.message}`)
-        return {output}
+
+        const sandboxUrl = await step.run('get-sandbox-url',async()=>{
+            const sandbox = await getSandbox(sandboxId)
+            const host = sandbox.getHost(3000)
+            return `https://${host}`
+        })
+        return {output,sandboxUrl}
     },
 );
